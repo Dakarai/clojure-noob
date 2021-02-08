e3394ac92b481b9d59dd48167a0b8c1a5cf5f62e
Brendan Davy <brendan@brendandavy.com>
2021-02-08 18:27:10 +0000

    added control flow

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -4,4 +4,18 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "I'm a little teapot!")
+  1
+  "a string"
+  ["a" "vector" "of" "strings"]
+  (if true
+    "By Zeus' Hammer!"
+    "By Aquaman's Trident!")
+  (if false
+    "By Odin's Elbow!")
+  (if true
+    (do (println "Success!")
+        "By Zeus' Hammer!")
+    (do (println "Failure!")
+        "By Aquaman's Trident!"))
+  )
